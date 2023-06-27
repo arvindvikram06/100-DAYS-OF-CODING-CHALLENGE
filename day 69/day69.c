82b5cb597cc7eb44631d8adae787456c06dac7f9
arvindvikram06 <126155802+arvindvikram06@users.noreply.github.com>
2023-06-28 00:45:45 +0530

    Create day69.c

---
+++ day 69/day69.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        if(n<10)
+        {
+            printf("Thanks for helping Chef!\n");
+        }
+        else
+        {
+            printf("-1\n");
+        }
+    }
+    return 0;
+}
